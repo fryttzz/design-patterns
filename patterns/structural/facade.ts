4e82fd38395d8be49aa2179b596a9061fc98279a
Matheus Santos <matheusps0012@gmail.com>
2022-06-28 09:55:23 -0300

    Add - Facade Pattern

---
+++ patterns/structural/facade.ts
@@ -0,0 +1,55 @@
+namespace facadePattern {
+    class Computer {
+        public getElecticShock() {
+            console.log('Ouch!');
+        }
+
+        public makeSound() {
+            console.log('Beep beep!');
+        }
+
+        public showLoadingScreen() {
+            console.log('Loading...');
+        }
+
+        public bam() {
+            console.log('Ready to be used!');
+        }
+
+        public closeEverything() {
+            console.log('Bup bup bup buzzzz!');
+        }
+
+        public sooth() {
+            console.log('Zzzzz');
+        }
+
+        public pullCurrent() {
+            console.log('Haaah!');
+        }
+    }
+    class ComputerFacade {
+        protected computer
+
+        public constructor(computer: Computer) {
+            this.computer = computer
+        }
+
+        public turnOn() {
+            this.computer.getElecticShock()
+            this.computer.makeSound()
+            this.computer.showLoadingScreen()
+            this.computer.bam()
+        }
+
+        public turnOf() {
+            this.computer.closeEverything()
+            this.computer.pullCurrent()
+            this.computer.sooth()
+        }
+    }
+
+    let computer = new ComputerFacade(new Computer())
+    computer.turnOn()
+    computer.turnOf()
+}
